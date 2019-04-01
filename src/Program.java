8de4fde225ee14ff032ee9e21fce93bf30beb61d
Alessandro D'Orazio <me@alessandrodorazio.it>
2019-04-01 18:26:25 +0200

    Main

--- src/Program.java
+++ src/Program.java
@@ -3,10 +3,14 @@ import veicolo.*;
 import pedaggio.*;
 import casello.*;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Program {
 
 
     public static void main(String[] args){
+
         /* Esempio veicoli, realizzati mediante array, ottimizzazione futura tramite connessione via database */
         Veicolo[] veicoli = new Veicolo[3];
         veicoli[0] = Veicolo.nuovoVeicolo("Smart", "ForTwo", 2014, "SM444RT", "Euro 4", 2, 3, 125); //leggero
@@ -20,17 +24,21 @@ public class Program {
         System.out.println("\n");
 
         /* Esempio caselli e autostrada */
-        Casello[] caselli = new Casello[3];
-        caselli[0] = new Casello("AQ Ovest", 300);
-        caselli[1] = new Casello("AQ Est", 305);
-        caselli[2] = new Casello("Assergi", 310);
+        Map caselli = new TreeMap();
+        caselli.put(300, "AQ Ovest");
+        caselli.put(305, "AQ Est");
+        caselli.put(310, "Assergi");
         System.out.println("Caselli");
-        System.out.println(caselli[0].toString());
-        System.out.println(caselli[1].toString());
-        System.out.println(caselli[2].toString());
+        System.out.println(caselli.size());
+
+        System.out.println(caselli.get(300));
+        System.out.println(caselli.get(305));
+        System.out.println(caselli.get(310));
         System.out.println("\n");
 
-        float[] tariffaUnitaria = {2f, 2.3f, 2.6f, 2.9f, 3.2f};
+        System.out.println(caselli.entrySet());
+
+        float[] tariffaUnitaria = {1f, 1.24f, 2f, 2.24f, 3f};
         Autostrada a14 = new Autostrada("Autostrada A14", caselli, true, 0f, 0f, tariffaUnitaria);
         System.out.println("Autostrade");
         System.out.println(a14.toString()); //informazioni sull'autostrada
@@ -38,9 +46,10 @@ public class Program {
 
         /* Esempio pedaggio */
         System.out.println("Pedaggi");
-        System.out.println("Pedaggio per il veicolo " + veicoli[0].targa + " per il tratto " + caselli[0].getName() + " - " + caselli[1].getName() + " è di " + Pedaggio.calcoloPedaggio(caselli[0], caselli[2], a14, veicoli[0]) + "€");
-        System.out.println("Pedaggio per il veicolo " + veicoli[1].targa + " per il tratto " + caselli[1].getName() + " - " + caselli[2].getName() + " è di " + Pedaggio.calcoloPedaggio(caselli[1], caselli[2], a14, veicoli[1]) + "€");
-
+        /*
+        System.out.println("Pedaggio per il veicolo " + veicoli[0].targa + " per il tratto " + caselli.get(300) + " - " + caselli.get(305) + " è di " + Pedaggio.calcoloPedaggio(caselli.get(0), caselli.get(2), a14, veicoli[0]) + "€");
+        System.out.println("Pedaggio per il veicolo " + veicoli[1].targa + " per il tratto " + caselli.get(305) + " - " + caselli.get(310) + " è di " + Pedaggio.calcoloPedaggio(caselli.get(1), caselli.get(2), a14, veicoli[1]) + "€");
+        */
     }
 
 }
