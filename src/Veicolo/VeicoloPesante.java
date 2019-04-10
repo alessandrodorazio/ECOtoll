0cd6ccad921017cbf009427727fb548aa98d7bd3
Alessandro D'Orazio <me@alessandrodorazio.it>
2019-04-10 19:35:39 +0200

    Fix calcolo della classe tariffaria

--- src/Veicolo/VeicoloPesante.java
+++ src/Veicolo/VeicoloPesante.java
@@ -7,7 +7,7 @@ public class VeicoloPesante extends Veicolo implements InterfacciaVeicolo {
     public VeicoloPesante(String modello, String marca, int anno, String targa, String classeAmbientale, int assi, int peso, int altezza){
         super(modello,marca,anno,targa, classeAmbientale, assi,peso,altezza);
         if(assi <= 2) throw new Error("Il numero di assi deve essere maggiore o uguale a 2!"); //un veicolo pesante deve avere almeno 2 assi
-        this.classeTariffaria = this.getClasseTariffaria();
+        this.classeTariffaria = this.calcoloClasseTariffaria();
     }
 
     public char calcoloClasseTariffaria() {
@@ -18,6 +18,6 @@ public class VeicoloPesante extends Veicolo implements InterfacciaVeicolo {
     public char getClasseTariffaria() { return this.classeTariffaria; }
 
     public int getClasseTariffariaInt() { return (this.assi>5)?5:this.assi; }
-    
+
 
 }
